0aff68aa869bcba4da404c032fbfe9ac12ed339e
Eduardo Melo <eduardo.melo.braga@gmail.com>
2023-03-28 22:11:06 -0300

    feat: added encoding and refined datasource

--- src/index.js
+++ src/index.js
@@ -31,10 +31,30 @@ const readDataset = async () => {
     return dataset;
 };
 
+const readEncoding = async (fileName) => {
+    const rawFileBytes = await new Promise(res => {
+        fs.readFile(path.join(__dirname, `./encoding/${fileName}`), (_, data) => res(data))
+    });
+
+    return JSON.parse(rawFileBytes.toString('utf8'));
+}
+
 const requestListener = async (req, res) => {
-    const dataset = await readDataset();
-    res.writeHead(200);
-    res.end(JSON.stringify(dataset, null, 4));
+    console.log('req url ', req.url)
+    if(req.url === '/phoneBrands/data') {
+        const dataset = await readDataset();
+        res.writeHead(200);
+        return res.end(JSON.stringify(dataset, null, 4));
+    }
+    
+    if(req.url === '/phoneBrands') {
+        const phoneBrandsEncoding = await readEncoding('phoneBrandsScatterplot.json');
+        res.writeHead(200);
+        return res.end(JSON.stringify(phoneBrandsEncoding, null, 4));
+    }
+
+    res.writeHead(404);
+    res.end(JSON.stringify({message: "Page doesn't exist."}, null, 4))
 };
 
 const server = http.createServer(requestListener);
