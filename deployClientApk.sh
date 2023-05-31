cb7f9fe3e507db8353a3f71e46da3ee33cb43911
Eduardo Braga <ext.eduardo.braga@farfetch.com>
2023-05-31 01:09:30 +0100

    feat: adjusted project to be deployed

---
+++ deployClientApk.sh
@@ -0,0 +1,3 @@
+# run with git bash
+cd ./src/client;
+eas build -p android --profile preview;
--- src/client/src/shared/api-clients/datasource-client.js
+++ src/client/src/shared/api-clients/datasource-client.js
@@ -1,4 +1,4 @@
-const ApiPath = 'http://192.168.1.82:8000';
+const ApiPath = 'http://situated-dashboard-backend.herokuapp.com';
 
 const get = (endpoint) => fetch(`${ApiPath}/${endpoint}`).then(res => res.json());
 
